de0497649d1f4077d5348bd96b8bc79bc1b8e54a
tuncersert <tuncaysert61@gmail.com>
2020-05-06 17:33:32 +0200

    Update Login.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -4,5 +4,6 @@ public class Login {
 
     public static void main(String[] args) {
         System.out.println("this is a test case");
+        System.out.println("why have not been shown");
     }
 }
