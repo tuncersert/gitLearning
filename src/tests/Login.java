893b8e4c4ed72e6a92447d09a958c22a7c8b353b
tuncersert <tuncaysert61@gmail.com>
2020-05-06 12:22:34 +0200

    add Login2.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -1,4 +1,8 @@
 package tests;
 
 public class Login {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test case");
+    }
 }
---
+++ src/tests/Login2.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class Login2 {
+
+    public static void main(String[] args) {
+        // this is a test also
+    }
+}
