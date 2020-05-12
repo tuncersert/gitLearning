987113fce15c881b8d24b94d7f40f502d0009ea1
tuncersert <62052976+tuncersert@users.noreply.github.com>
2020-05-12 17:06:39 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -3,5 +3,7 @@ package tests;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("practice");
+        
+        //tuesday class..this line is added from github
     }
 }
